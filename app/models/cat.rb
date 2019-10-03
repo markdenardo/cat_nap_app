9a9d03de2886737a0cc8112e2a7019dcaf494cb4
markdenardo <mark.denardo@gmail.com>
2019-10-02 21:19:19 -0400

    seeded

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,7 @@
 class Cat < ApplicationRecord
   has_many :naps
   has_many :users, through: :naps
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
 
 end
